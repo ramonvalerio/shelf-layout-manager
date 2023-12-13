564d5b1e678c4e6064fa136187f7dfb20a3356c6
Ramon Valerio da  Silva <ramon.dasilva@southworks.com>
2023-12-13 17:16:12 +0900

    create SKU entity

---
+++ ShelfLayoutManager.Core/Domain/SKUs/ProductSize.cs
@@ -0,0 +1,9 @@
+﻿namespace ShelfLayoutManager.Core.Domain.SKUs
+{
+    public class ProductSize
+    {
+        public float Width { get; set; }
+        public float Depth { get; set; }
+        public float Height { get; set; }
+    }
+}
---
+++ ShelfLayoutManager.Core/Domain/SKUs/SKU.cs
@@ -0,0 +1,13 @@
+﻿namespace ShelfLayoutManager.Core.Domain.SKUs
+{
+    public class SKU
+    {
+        public string JanCode { get; set; }
+        public string Name { get; set; }
+        public string DrinkSize { get; set; }
+        public ProductSize Size { get; set; }
+        public string ShapeType { get; set; }
+        public string ImageUrl { get; set; } 
+        public DateTime TimeStamp { get; set; }
+    }
+}
